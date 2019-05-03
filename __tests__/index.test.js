50fe2976f7fd3064f5bf456b2456d7b59a79abaf
Adam Onishi <onishiweb@gmail.com>
2019-05-03 17:20:35 +0100

    Update general test script to run only visual tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,19 +6,11 @@ let chromeless = null
 beforeAll(() => { chromeless = setup() })
 afterAll(async () => { await teardown(chromeless) })
 
-test('+++ home renders correctly (visual)', async () => {
+test('landing page renders correctly (visual)', async () => {
   const screenshotPath = await chromeless.goto(global.config.baseUrl).wait('button').screenshot()
   const screenshot = await getFile(screenshotPath)
   const customDiffConfig = {
     composeLeftToRight: true,
   }
   expect(screenshot).toMatchImageSnapshot({ customDiffConfig })
-})
-
-test('+++ home renders correctly', async () => {
-  const html = await chromeless
-    .goto(global.config.baseUrl)
-    .wait('button')
-    .evaluate(() => document.querySelector('button').innerHTML)
-  expect(html).toMatchSnapshot()
 })
